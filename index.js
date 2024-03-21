26dd246bbab4db4468e0d39178bbba1340771b18
Derrick <riobad65@gmail.com>
2024-03-21 14:38:22 +0300

    function lab test

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+// Test one
+function distanceFromHqInBlocks(Destination){
+   const HQ = 42;
+   return Math.abs(Destination - HQ);
+}
+//Test two
+function distanceFromHqInFeet(Destination){
+    const feet = 264;
+    const Block = distanceFromHqInBlocks(Destination);
+    return Block * feet;
+    
+}
+//Test three
+function distanceTravelledInFeet(start,end){
+    const feet = 264;
+    const Distance = Math.abs(start - end)
+    return Distance * feet;
+}
+//Test four
+function calculatesFarePrice(start,end){
+     const travel= distanceTravelledInFeet(start,end)
+
+     if(travel <= 400){
+        return 0;
+     } 
+     if( travel  > 400 && travel <= 2000 ){
+        return 2.56;
+     } 
+     if(travel > 2000 && travel <= 2500){
+        return 25;
+     }  
+     else{
+        return 'cannot travel that far';
+     }
+   
+}
